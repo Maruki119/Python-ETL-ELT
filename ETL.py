07e067c525dc60126d876c0b967f17750aae8d7b
Maruki119 <93820527+Maruki119@users.noreply.github.com>
2026-06-14 00:27:59 +0700

    ETL world economites GDP from website

--- ETL.py
+++ ETL.py
@@ -1,1 +1,100 @@
-print("Hello World")
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import sqlite3
+import numpy as np
+from datetime import datetime
+
+def extract(url, table_attribs):
+    page = requests.get(url).text
+    data = BeautifulSoup(page, 'html.parser')
+    df = pd.DataFrame(columns = table_attribs)
+    tables = data.find_all('tbody')
+    rows = tables[2].find_all('tr')
+
+    for row in rows:
+        col = row.find_all('td')
+        if len(col) != 0:
+            if col[0].find('a') is not None and '—'not in col[2]:
+                data_dict = {'Country': col[0].a.contents[0],
+                             'GDP_USD_millions': col[2].contents[0]}
+                df1 = pd.DataFrame(data_dict, index=[0])
+                df = pd.concat([df, df1], ignore_index=True)
+
+    return df
+
+def transform(df):
+    GDP_list = df["GDP_USD_millions"].tolist()
+    print(GDP_list)
+    GDP_list = [float("".join(x.split(','))) for x in GDP_list]
+    GDP_list = [np.round(x/1000, 2) for x in GDP_list]
+    df['GDP_USD_millions'] = GDP_list
+    df = df.rename(columns = {"GDP_USD_millions":"GDP_USD_billions"})
+
+    return df
+
+def load_to_csv(df, csv_path):
+    df.to_csv(csv_path)
+
+def load_to_db(df, sql_connection, table_name):
+    df.to_sql(table_name, sql_connection, if_exists='replace', index=False)
+
+def run_query(query_statement, sql_connection):
+    print(query_statement)
+    query_output = pd.read_sql(query_statement, sql_connection)
+    print(query_output)
+
+def log_progress(message):
+    timestamp_format = '%Y-%h-%d-%H:%M:%S' # Year-Monthname-Day-Hour-Minute-Second 
+    now = datetime.now()
+    timestamp = now.strftime(timestamp_format) 
+    with open("./etl_project_log.txt","a") as f: 
+        f.write(timestamp + ' : ' + message + '\n')
+
+
+
+url = 'https://web.archive.org/web/20230902185326/https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29'
+table_attribs = ["Country", "GDP_USD_millions"]
+db_name = 'World_Economies.db'
+table_name = 'Countries_by_GDP'
+csv_path = 'Countries_by_GDP.csv'
+
+print('Start Extract data')
+log_progress('Start Extract data')
+extract_data = extract(url, table_attribs)
+log_progress('End Extract data')
+print('End Extract data')
+
+print('Start Transform data')
+log_progress('Start Transform data')
+transform_data = transform(extract_data)
+log_progress('End Transform data')
+print('End Transform data')
+
+print('Start Load data to csv')
+log_progress('Start Load data to csv')
+load_to_csv(transform_data, csv_path)
+log_progress('End Load data to csv')
+print('End Load data to csv')
+
+print('Connect to DB')
+log_progress('Connect to DB')
+conn = sqlite3.connect(db_name)
+log_progress('DB is connected')
+print('DB is connected')
+
+print('Start Load data to db')
+log_progress('Start Load data to db')
+load_to_db(transform_data, conn, table_name)
+log_progress('End Load data to db')
+print('End Load data to db')
+
+print('Start Query from db')
+log_progress('Start Query from db')
+run_query(f"SELECT * from {table_name} WHERE GDP_USD_billions >= 100", conn)
+log_progress('End Query from db')
+print('End Query from db')
+
+log_progress('Process Complete.')
+
+conn.close()
