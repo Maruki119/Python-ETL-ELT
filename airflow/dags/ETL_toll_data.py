a1b277e138b615eb6f73cf9784c588d33853c103
Maruki119 <93820527+Maruki119@users.noreply.github.com>
2026-06-21 19:48:18 +0700

    Add mini project (ETL_toll_data) using DAGS

---
+++ airflow/dags/ETL_toll_data.py
@@ -0,0 +1,82 @@
+from datetime import datetime, timedelta
+import pendulum
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+
+destination_dir = "/opt/airflow/dags/finalassignment"
+
+default_args = {
+    "owner": "Nititorn Kitprasopchok",
+    "email": "nititorn.kij@gmail.com",
+    "email_on_failure": True,
+    "email_on_retry": True,
+    "start_date": pendulum.today("UTC").add(days=-1),
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+with DAG(
+    dag_id="ETL_toll_data",
+    default_args=default_args,
+    description="Apache Airflow Final Assignment",
+    schedule=timedelta(days=1),
+) as dag:
+    
+    unzip_data = BashOperator(
+        task_id="unzip_data",
+        bash_command="tar -xvf " + destination_dir + "/tolldata.tgz -C " + destination_dir,
+        dag=dag
+    )
+    
+    extract_data_from_csv = BashOperator(
+        task_id="extract_data_from_csv",
+        bash_command=f"""
+            echo "Rowid,Timestamp,Anonymized Vehicle Number,Vehicle Type" > {destination_dir}/csv_data.csv
+            cut -d',' -f1-4 {destination_dir}/vehicle-data.csv >> {destination_dir}/csv_data.csv
+        """,
+        dag=dag
+    )
+    
+    extract_data_from_tsv = BashOperator(
+        task_id="extract_data_from_tsv",
+        bash_command=f"""
+            echo "Number of axles,Tollplaza id, Tollplaza code" > {destination_dir}/tsv_data.csv
+            cut -f5-7 {destination_dir}/tollplaza-data.tsv \
+            | tr '\\t' ',' \
+            | tr -d '\\r' \
+            > {destination_dir}/tsv_data.csv
+        """,
+        dag=dag
+    )
+    
+    extract_data_from_fixed_width = BashOperator(
+        task_id="extract_data_from_fixed_width",
+        bash_command=f"""
+            echo "Type of Payment code, Vehicle Code" > {destination_dir}/fixed_width_data.csv
+            awk '{{print substr($0,59,3) "," substr($0,63,5)}}' \
+            {destination_dir}/payment-data.txt \
+            >> {destination_dir}/fixed_width_data.csv
+        """,
+        dag=dag
+    )
+    
+    consolidate_data = BashOperator(
+        task_id="consolidate_data",
+        bash_command=f"""
+            paste -d',' {destination_dir}/csv_data.csv {destination_dir}/tsv_data.csv {destination_dir}/fixed_width_data.csv > {destination_dir}/extracted_data.csv
+        """,
+        dag=dag
+    )
+    
+    transform_data = BashOperator(
+        task_id="transform_data",
+        bash_command=f"""
+            awk -F',' 'BEGIN {{OFS=","}} NR==1 {{print; next}} {{$4=toupper($4); print}}' \
+            {destination_dir}/extracted_data.csv \
+            > {destination_dir}/staging/transformed_data.csv
+        """,
+        dag=dag
+    )
+    
+unzip_data >> extract_data_from_csv >> extract_data_from_tsv >> extract_data_from_fixed_width >> consolidate_data >> transform_data
